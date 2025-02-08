fe8f93b7e391363677c0e0a6b03b0e39c8edc053
Petr Gregor <petr.gregor@email.cz>
2025-02-08 20:54:57 +0100

    Home page.

--- viewer/views.py
+++ viewer/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from viewer.models import Movie
 
 
+def home(request):
+    return render(request, "home.html")
+
+
 def movies(request):
     movies_ = Movie.objects.all()
     context = {'movies': movies_}
